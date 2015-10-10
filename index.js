f75c54215cdffa32712cbc940c1ea94a0ea6eceb
Anthony Lai <yukuan.anthony.lai@gmail.com>
2015-10-10 10:04:34 -0700

    Get mongoDB to work

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+//var jsonfile = require('jsonfile');
 var app = express();
+//var file = 'data.json';
+
+//	mongodb
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
+var ObjectId = require('mongodb').ObjectID;
+
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	db.close();
+});
+
+
+//	testing mongodb
+/*
+var user = "Andy Lin";
+var friendsArray = ["Jayendra Jog", "Helen Lee", "Anthony Lai", "Josephine Ree", "Rex Fukuchi"];
+var scheduleArray = "test";
+var freeTimeArray = ["04:00", "06:00", "16:00"];
+
+var insertDocument = function(db, callback) {
+	db.collection('testUsers').insertOne( {
+		user : {
+			"friends" : friendsArray,
+			"schedule" : scheduleArray,
+			"freeTime" : freeTimeArray
+		}
+	}, function(err, result) {
+	assert.equal(err, null);
+	console.log("Inserted a document into the users collection.");
+	callback(result);
+	});
+};
+
+MongoClient.connect(url, function(err, db) {
+	assert.equal(err, null);
+	insertDocument(db, function() {
+		db.close();
+	});
+});
+
+var findTestUsers = function(db, callback) {
+   var cursor =db.collection('testUsers').find( );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         console.dir(doc);
+      } else {
+         callback();
+      }
+   });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  findTestUsers(db, function() {
+      db.close();
+  });
+});
+*/
 
 
 //	set
@@ -14,10 +77,10 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 //	http://stackoverflow.com/questions/5710358/how-to-get-post-a-query-in-express-js-node-js
 //app.use(bodyParser.json());	// to support JSON-encoded bodies
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(express.json());	// to support JSON-encoded bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 
 
 //	get
@@ -33,22 +96,94 @@ app.get('/form', function(req, res) {
 	res.render('form');
 });
 
+app.get('/retrieve', function(req, res) {
+	console.log("In retrieve");
+
+	var findUsers = function(db, callback) {
+   		var cursor =db.collection('users').find( );
+   		cursor.each(function(err, doc) {
+	      	assert.equal(err, null);
+	      	if (doc != null) {
+	         	console.dir(doc);
+	      	} else {
+	         	callback();
+	      	}
+   		});
+	};
+
+	MongoClient.connect(url, function(err, db) {
+  		assert.equal(null, err);
+  		findUsers(db, function() {
+      		db.close();
+  		});
+	});
+});
+
 //	post
 app.post('/submit', function(req, res) {
+	console.log("In submit");
 	//res.send("works?");
 	var name = req.body.name,
-		someField = req.body.someField;
-	console.log("Name is " + name + ", and someField is " + someField);
+		schedule = req.body.schedule;
+	console.log("Name is " + name + ", and scheduleArray is " + schedule);
 	
-	if (JSON.stringify(files.upload.name) == null) {
-      console.log("Error no file submitted");
-    } else {
-    	var myObject = [{"name": " ", "someField": " "}];
-    	myObject[0].name = name;
-    	myObject[0].someField = someField;
-    	console.log(myObject);
-    }
-    
+	var insertDocument = function(db, callback) {
+		db.collection('users').insertOne( {
+			user : {
+				"Name" : name,
+				"schedule" : schedule
+			}
+		}, function(err, result) {
+			assert.equal(err, null);
+			console.log("Inserted a document into the users collection.");
+			callback(result);
+		});
+	};
+
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(err, null);
+		insertDocument(db, function() {
+			db.close();
+		});
+	});
+
+	// var findUsers = function(db, callback) {
+ //   		var cursor =db.collection('testUsers').find( );
+ //   		cursor.each(function(err, doc) {
+	//       	assert.equal(err, null);
+	//       	if (doc != null) {
+	//          	console.dir(doc);
+	//       	} else {
+	//          	callback();
+	//       	}
+ //   		});
+	// };
+
+	// MongoClient.connect(url, function(err, db) {
+ //  		assert.equal(null, err);
+ //  		findUsers(db, function() {
+ //      		db.close();
+ //  		});
+	// });
+	// if (JSON.stringify(files.upload.name) == null) {
+ //      console.log("Error no file submitted");
+ //    } else {
+ //    	var myObject = [{"name": " ", "someField": " "}];
+ //    	myObject[0].name = name;
+ //    	myObject[0].someField = someField;
+ //    	console.log(myObject);
+ //    }
+
+ 	// var myObject = [{"name": " ", "someField": " "}];
+  //    	myObject[0].name = name;
+  //    	myObject[0].someField = someField;
+  	// var myObject = [{name: 'JP'}];
+
+   //  jsonfile.readFile(file, function(err, myObject) {
+   //  	//console.dir(obj);
+   //  	console.log("Is this working");
+   //  	jsonfile.writeFileSync(file, myObject);
+   //  });
 
 });
 
