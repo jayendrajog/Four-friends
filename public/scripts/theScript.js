4cf057552ca1995bcea5c9fced05d474aba5e09a
Jayendra <jay08jog@gmail.com>
2015-10-10 04:46:52 -0700

    user authentification works

--- public/scripts/theScript.js
+++ public/scripts/theScript.js
@@ -1,3 +1,18 @@
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  function fb_login() {
+	FB.login( function(response) {statusChangeCallback(response);}, { scope: 'email,public_profile,user_friends' } );
+
+	}
+
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
@@ -7,9 +22,10 @@
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
+      // Logged into your app and Facebook. 
+      window.location = "https://www.google.com/";
+    } 
+    else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
@@ -20,6 +36,7 @@
         'into Facebook.';
     }
   }
+  
 
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
@@ -35,9 +52,12 @@
     appId      : '521096238056941',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
+    status 	   : true, 
     xfbml      : true,  // parse social plugins on this page
     version    : 'v2.2' // use version 2.2
   });
+
+
 
   // Now that we've initialized the JavaScript SDK, we call 
   // FB.getLoginStatus().  This function gets the state of the
@@ -51,33 +71,13 @@
   //
   // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
+	// FB.getLoginStatus(function(response) {
+ //    	statusChangeCallback(response);
+ //  	});
 
   };
 
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
   
-  function fb_login() {
-	FB.login( 
-		function() {}, { scope: 'email,public_profile,user_friends' } );
-	}
+
+
+	
